dfbe3ddc751defc139b6212ac04d40a8f97fe3c7
harikeerthpv07 <harikeerthpv07@gmail.com>
2025-03-16 17:10:01 +0530

    Project Completed

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,28 @@
+import express, { Router } from "express";
+import {
+  login,
+  logout,
+  signup,
+  updateProfile,
+  checkAuth,
+} from "../controllers/auth.controller.js";
+
+import { sendMessage } from "../controllers/message.controller.js";
+
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
+
+router.post("/send/:id", protectRoute, sendMessage);
+
+export default router;
